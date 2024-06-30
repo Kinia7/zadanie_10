50931c896797b311dd06db1e993a830468775abf
Kinga Olszewska <150083424+Kinia7@users.noreply.github.com>
2024-06-30 12:50:20 +0200

    Update urls.py

--- quotes_project/guotes_project/urls.py
+++ quotes_project/guotes_project/urls.py
@@ -1,14 +1,3 @@
-# quotes/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('register/', views.register, name='register'),
-    path('add_author/', views.add_author, name='add_author'),
-    path('add_quote/', views.add_quote, name='add_quote'),
-    path('author/<int:author_id>/', views.author_detail, name='author_detail'),
-]
 
 # quotes_project/urls.py
 from django.contrib import admin
